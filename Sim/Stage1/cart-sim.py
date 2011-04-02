7c56fd9c3e1b304831106bc1ef282611ed922e23
Max Lapan <max.lapan@gmail.com>
2011-04-03 01:23:33 +0400

    Simulation seems correct. Function is wrong

--- Sim/Stage1/cart-sim.py
+++ Sim/Stage1/cart-sim.py
@@ -3,10 +3,10 @@
 import math
 
 l = 0.3                           # distance between back wheel and mass center in meters
-alpha = 10.0                      # angle of wheel
+alpha = 20.0                      # angle of wheel
 vel = 0.1                         # velocity in m/s
 delta_t = 0.1                     # time interval in seconds
-max_t = 5                         # time to simulate
+max_t = 20                        # time to simulate
 
 # consts
 sin_a = math.sin (math.radians (alpha))
@@ -19,21 +19,29 @@ phi = 0.0                         # initial angle in degrees
 
 t = 0
 
-A_x = A_y = []
-O_x = A_y = []
+A_data = []
+O_data = []
 Phi = []
 
 
+def dump_pts (name, pts):
+    f = open (name, 'w')
+    for ox,oy in pts:
+        f.write ("%f %f\n" % (ox, oy))
+    f.close ()
+    
+
 while t < max_t:
     t += delta_t
-    phi = math.degrees (math.asin (t * vel * sin_a * sin_a))
-    A = (t * vel * math.sin (math.radians (alpha + phi)), 
-         t * vel * math.cos (math.radians (alpha + phi)))
-    O = (t * vel * cos_a * math.sin (math.radians (phi)),
-         t * vel * cos_a * math.cos (math.radians (phi)))
-
+    A = (O[0] + l * math.sin (math.radians (-phi)),
+         O[1] - l * math.cos (math.radians (-phi)))
+    O = (O[0] + delta_t * vel * cos_a * math.sin (math.radians (phi)),
+         O[1] + delta_t * vel * cos_a * math.cos (math.radians (phi)))
+    phi += -math.degrees (delta_t * vel * sin_a / l)
     print "%.2f: phi=%.2f, A=(%.4f, %.4f), O=(%.4f, %.4f)" %  (t, phi, A[0], A[1], O[0], O[1])
-    Phi.append (phi)
+    O_data.append (O)
+    A_data.append (A)
+#    Phi.append (phi)
 
-print
-print "Phi = " + str (Phi)
+dump_pts ("O.dat", O_data)
+dump_pts ("A.dat", A_data)
