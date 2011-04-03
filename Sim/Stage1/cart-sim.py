ed153bd5a1cd60a1cfaa1ccb625f0a79e8a5fce1
Max Lapan <max.lapan@gmail.com>
2011-04-03 21:37:26 +0400

    Solution found

--- Sim/Stage1/cart-sim.py
+++ Sim/Stage1/cart-sim.py
@@ -6,7 +6,7 @@ l = 0.3                           # distance between back wheel and mass center 
 alpha = 20.0                      # angle of wheel
 vel = 0.1                         # velocity in m/s
 delta_t = 0.1                     # time interval in seconds
-max_t = 20                        # time to simulate
+max_t = 60                        # time to simulate
 
 # consts
 sin_a = math.sin (math.radians (alpha))
@@ -41,7 +41,8 @@ while t < max_t:
     print "%.2f: phi=%.2f, A=(%.4f, %.4f), O=(%.4f, %.4f)" %  (t, phi, A[0], A[1], O[0], O[1])
     O_data.append (O)
     A_data.append (A)
-#    Phi.append (phi)
+    Phi.append ((t, phi))
 
 dump_pts ("O.dat", O_data)
 dump_pts ("A.dat", A_data)
+dump_pts ("Phi.dat", Phi)
